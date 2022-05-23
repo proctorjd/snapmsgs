513e9500c8270e347249b9bd9a45ff9d59bc06bf
Paula <pauladzuck@hotmail.com>
2022-05-22 19:57:34 -0500

    Adding POC-idea using web sockets.

---
+++ POC/Form1.cs
@@ -0,0 +1,10 @@
+namespace POC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ POC_Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WebSocketSharp.Server;
+
+namespace POC_Server
+{
+    class Program
+    {
+        public static readonly WebSocketServer ServerSocket = new("ws://localhost:9876");
+      
+
+        //Getting server online
+        static void Main(string[] args)
+        {
+            ServerSocket.Start();
+            Console.WriteLine("Server started. Press any key to stop the server.");
+            Console.ReadKey();
+            ServerSocket.Stop();
+
+        }
+
+    }
+
+
+
+}
