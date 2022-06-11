842502880a4de3f3a4ed2460fca1c45e34810686
Paula <pauladzuck@hotmail.com>
2022-06-10 19:21:04 -0500

    Adding more updates to repo.

--- POC_Server/Chat.cs
+++ POC_Server/Chat.cs
@@ -1,4 +1,6 @@
 ﻿
+using Newtonsoft.Json;
+using POC.Core;
 using System;
 using System.Text;
 using WebSocketSharp;
@@ -8,28 +10,11 @@ using WebSocketSharp.Server;
 namespace POC_Server
 {
     public class Chat: WebSocketBehavior
+
     {
         protected override void OnOpen()
         {
             Console.WriteLine($"Client Connected: {ID}");
-
-            //var output = new StringBuilder()
-           // .AppendFormat("There are so many users logged in!", Sessions.Count).AppendLine()
-            //.AppendLine();
-
-           // foreach(var session in Sessions.Sessions)
-           // {
-             //   if (session.ID == ID)
-                 //   continue;
-
-              //  output.AppendLine(session.ID);
-           // }
-
-          //  Send(output.ToString());
-
-           // Sessions.Broadcast($"User joined servier ID: {ID}");
-
-
 
         }
 
@@ -52,10 +37,53 @@ namespace POC_Server
             .AppendFormat("{0} - {1}:", DateTime.UtcNow.ToString("mm: hh tt"), ID).AppendLine()
             .AppendLine(e.Data);
 
-
-            Sessions.Broadcast(output.ToString());
+            ParseJson(e.Data);
+            //Sessions.Broadcast(output.ToString());
             ///Sessions.Broadcast(e.Data);
 
+        }
+
+        private string Header ()
+        {
+            return String.Format("{0} - {1}:", DateTime.UtcNow.ToString("mm: hh tt"), ID);
+        }
+
+        private void Broadcast()
+        {
+
+        }
+
+        private void ParseJson(string json)
+        {
+            var request = JsonConvert.DeserializeObject<Request>(json);
+
+            switch (request.Op)
+            {
+                case OpCode.Hello:
+                    //var helloRequest = JsonConvert.DeserializeObject<Hello>(json);
+
+
+                    var output = new StringBuilder()
+                    .AppendFormat("There are so many users logged in!", Sessions.Count).AppendLine()
+                    .AppendLine();
+
+                     foreach(var session in Sessions.Sessions)
+                     {
+                        if (session.ID == ID)
+                        continue;
+
+                        output.AppendLine(session.ID);
+                     }
+
+                      Send(output.ToString());
+
+                    Sessions.Broadcast($"User joined servier ID: {ID}");
+
+                    break;
+                default:
+                    break;
+
+            }
 
         }
     }
